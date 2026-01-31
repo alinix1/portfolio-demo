e51f2d4878cbc6717346e518f734142dbc0d684b
Ali Nix <anix8605@gmail.com>
2026-01-30 17:40:23 -0700

    update project item with overlay container for better color contrast

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -14,19 +14,19 @@ const ProjectItem = ({ title, backgroundImg, projectUrl }) => {
           style={{ objectFit: "cover" }}
         />
       </div>
-      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 className="text-2xl text-white tracking-wider text-center">
-          {title}
-        </h3>
-        <p className="pb-4 pt-2 text-white text-center">
-          React JS / Full Stack
-        </p>
-        <Link href={projectUrl} aria-label="View Project">
-          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
-            More Info
+      <Link href={projectUrl} aria-label="View Project">
+        <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-black/50 px-4 py-1 rounded">
+          <h3 className="text-2xl text-white tracking-wider text-center">
+            {title}
+          </h3>
+          <p className="pb-4 pt-2 text-white text-center">
+            React JS / Full Stack
           </p>
-        </Link>
-      </div>
+          {/* <p className="text-center text-gray-800 font-bold text-lg cursor-pointer">
+            More Info
+          </p> */}
+        </div>
+      </Link>
     </div>
   );
 };
